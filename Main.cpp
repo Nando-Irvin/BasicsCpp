2c2d7e56531e0b21b07bb2001124775d60928a2c
Fernando Irvin <89152049+Nando-Irvin@users.noreply.github.com>
2021-08-18 11:47:41 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
+
+int main()
+{
+	int var = 8;
+	int* ptr = &var;
+	*ptr = 10;
+	LOG(var);
+
+	char* buffer = new char[8];
+	memset(buffer, 0, 8);
+
+	char** prtr = &buffer;
+
+	delete[] buffer;
+	std::cin.get();
+}
+
